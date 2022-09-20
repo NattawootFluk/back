bf07544ff7f062528ddeeb787bf2d466ca49fb11
Fluk <64309010013@bncc.ac.th>
2022-09-21 01:43:40 +0700

    h

--- index.php
+++ index.php
@@ -14,40 +14,153 @@
         <div class="row justify-content-end">
             <a href="addpage.php" class="col-3 btn btn-lg btn-primary m-3 text-center">Add</a>
         </div>
-        <div class="row p-3">
-            <p class="h3">ภาคกลาง</p>
+        <div class="row p-3 d-flex justify-content-between data-hide1">
+            <p class="h3 col-6">ภาคกลาง</p>
+            <button onclick="showdata1()" class="btn col-1 btn-info" id='show'>Show</button>
         </div>
-        <div class="row p-3">
-            <div class="card col-lg-6 col-12 text-center p-2">
-                <div class="a">
+        <div class="row p-3 data-show1">
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class=" text-center p-2">
                     <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
-                    <p class='h4'>hello</p>
-                </div>
+                    <p class='h4 m-3'>hello</p>
             </div>
-            <div class="card col-lg-6 col-12 text-center p-2">
-                <div class="">
+            </a>
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class="text-center p-2">
                     <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
-                    <p class='h4'>hello</p>
-                </div>
+                    <p class='h4 m-3'>hello</p>
             </div>
+            </a>
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class="text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
         </div>
-        <div class="row p-3">
-            <p class="h3">ภาคเหนือ</p>
+        <div class="row p-3 d-flex justify-content-between data-hide2">
+            <p class="h3 col-6">ภาคเหนือ</p>
+            <button onclick="showdata2()" class="btn col-1 btn-info" id='show2'>Show</button>
         </div>
-        <div class="row p-3">
-            <p class="h3">ภาคตะวันออกเฉียงเหนือ</p>
+        <div class="row p-3 data-show2">
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class=" text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class="text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class="text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
         </div>
-        <div class="row p-3">
-            <p class="h3">ภาคตะวันตก</p>
+        <div class="row p-3 d-flex justify-content-between">
+            <p class="h3 col-6">ภาคตะวันออกเฉียงเหนือ</p>
+            <button onclick="showdata3()" class="btn col-1 btn-info" id='show3'>Show</button>
         </div>
-        <div class="row p-3">
-            <p class="h3">ภาคตะวันออก</p>
+        <div class="row p-3 data-show3">
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class=" text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class="text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class="text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
         </div>
-        <div class="row p-3">
-            <p class="h3">ภาคใต้</p>
+        <div class="row p-3 d-flex justify-content-between data-hide4">
+            <p class="h3 col-6">ภาคตะวันตก</p>
+            <button onclick="showdata4()" class="btn col-1 btn-info" id='show4'>Show</button>
+        </div>
+        <div class="row p-3 data-show4">
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class=" text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class="text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class="text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
+        </div>
+        <div class="row p-3 d-flex justify-content-between">
+            <p class="h3 col-6">ภาคตะวันออก</p>
+            <button onclick="showdata5()" class="btn col-1 btn-info" id='show5'>Show</button>
+        </div>
+        <div class="row p-3 data-show5">
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class=" text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class="text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class="text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
+        </div>
+        <div class="row p-3 d-flex justify-content-between">
+            <p class="h3 col-6">ภาคใต้</p>
+            <button onclick="showdata6()" class="btn col-1 btn-info" id='show6'>Show</button>
+        </div>
+        <div class="row p-3 data-show6">
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class=" text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class="text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
+            <a href="info.php" class="col-lg-4 col-md-6 col-sm-12">
+            <div class="text-center p-2">
+                    <img src="https://photo.maahalai.com/wp-content/uploads/2018/02/PicsArt-Full-Moon.jpg">
+                    <p class='h4 m-3'>hello</p>
+            </div>
+            </a>
         </div>        
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="main.js"></script>
 </body>
 </html>
